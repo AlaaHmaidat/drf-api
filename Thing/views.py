6fe48ce28645aab040856aefb2503628849ef847
AlaaHmaidat <alaahmidat2000@gmail.com>
2023-07-13 19:19:53 +0000

    Django REST Framework & Docker

--- Thing/views.py
+++ Thing/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Thing
+from .serializers import Thingserializer
 # Create your views here.
+
+# ListAPIView
+class Thing_list(generics.ListCreateAPIView):
+    queryset = Thing.objects.all()
+    serializer_class = Thingserializer
+
+# RetrieveAPIView
+class Thing_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Thing.objects.all()
+    serializer_class = Thingserializer
